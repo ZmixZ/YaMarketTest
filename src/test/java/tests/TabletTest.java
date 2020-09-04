459387be53110c7c079debed8ce6e5c59a4b726d
ZmixZ <termixator@yandex.ru>
2020-09-04 18:03:54 +0300

    Session 5

--- src/test/java/tests/TabletTest.java
+++ src/test/java/tests/TabletTest.java
@@ -1,12 +1,10 @@
 package tests;
 
 import manager.TestBase;
-import model.Tires;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TabletTest extends TestBase {
@@ -15,8 +13,14 @@ public class TabletTest extends TestBase {
     public void testTablet() throws InterruptedException {
         app.enterWebsite();
         app.driver.findElement(By.xpath("//span[text()='Электроника']/..")).click();
-        app.driver.findElement(By.xpath("//a[text()='Планшеты']")).click();
-        List tablet = app.driver.findElements(By.xpath("//h3[./a[@class='_27nuSZ19h7 wwZc93J2Ao cia-cs']]"));
-        Thread.sleep(10000);
+        app.driver.findElement(By.xpath("//a[text()='Ретро консоли']")).click();
+        List<WebElement> tablet = app.driver.findElements(By.xpath("//h3[@class='_3dCGE8Y9v3 _25uOAS5e4A']//a"));
+        for(WebElement i : tablet){
+            if(i.getText().equals("Игровая приставка Genesis Gopher 2 (500 игр)")){
+                app.driver.findElement(By.linkText("Игровая приставка Genesis Gopher 2 (500 игр)")).click();}
+        }
+        app.getSpecificationHelper().getWindow(1);
+        app.getSearchHelper().clickCharacteristic();
+        Thread.sleep(8000);
     }
 }

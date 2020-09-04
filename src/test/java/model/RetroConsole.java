b3ceb78b090b1e7a4efbae8762c5fa6454940b57
ZmixZ <termixator@yandex.ru>
2020-09-04 21:42:48 +0300

    Session 6

--- src/test/java/model/RetroConsole.java
+++ src/test/java/model/RetroConsole.java
@@ -1,5 +1,184 @@
 package model;
 
+import java.util.Objects;
+
 public class RetroConsole {
 
+    String formFactor;
+    String form;
+    String screenDiagonal;
+    String screenResolution;
+    String architecture;
+    String mediaType;
+    String memoryCardSlot;
+    String preinstallGames;
+    String battery;
+    String batteryCapacity;
+    String length;
+    String width;
+    String height;
+    String weight;
+    String anotherInformation;
+
+    public String getFormFactor() {
+        return formFactor;
+    }
+
+    public RetroConsole withFormFactor(String formFactor) {
+        this.formFactor = formFactor;
+        return this;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public RetroConsole withForm(String form) {
+        this.form = form;
+        return this;
+    }
+
+    public String getScreenDiagonal() {
+        return screenDiagonal;
+    }
+
+    public RetroConsole withScreenDiagonal(String screenDiagonal) {
+        this.screenDiagonal = screenDiagonal;
+        return this;
+    }
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+
+    public RetroConsole withScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+        return this;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public RetroConsole withArchitecture(String architecture) {
+        this.architecture = architecture;
+        return this;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public RetroConsole withMediaType(String mediaType) {
+        this.mediaType = mediaType;
+        return this;
+    }
+
+    public String getMemoryCardSlot() {
+        return memoryCardSlot;
+    }
+
+    public RetroConsole withMemoryCardSlot(String memoryCardSlot) {
+        this.memoryCardSlot = memoryCardSlot;
+        return this;
+    }
+
+    public String getPreinstallGames() {
+        return preinstallGames;
+    }
+
+    public RetroConsole withPreinstallGames(String preinstallGames) {
+        this.preinstallGames = preinstallGames;
+        return this;
+    }
+
+    public String getBattery() {
+        return battery;
+    }
+
+    public RetroConsole withBattery(String battery) {
+        this.battery = battery;
+        return this;
+    }
+
+    public String getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public RetroConsole withBatteryCapacity(String batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+        return this;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public RetroConsole withLength(String length) {
+        this.length = length;
+        return this;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public RetroConsole withWidth(String width) {
+        this.width = width;
+        return this;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public RetroConsole withHeight(String height) {
+        this.height = height;
+        return this;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public RetroConsole withWeight(String weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public String getAnotherInformation() {
+        return anotherInformation;
+    }
+
+    public RetroConsole withAnotherInformation(String anotherInformation) {
+        this.anotherInformation = anotherInformation;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RetroConsole that = (RetroConsole) o;
+        return Objects.equals(formFactor, that.formFactor) &&
+                Objects.equals(form, that.form) &&
+                Objects.equals(screenDiagonal, that.screenDiagonal) &&
+                Objects.equals(screenResolution, that.screenResolution) &&
+                Objects.equals(architecture, that.architecture) &&
+                Objects.equals(mediaType, that.mediaType) &&
+                Objects.equals(memoryCardSlot, that.memoryCardSlot) &&
+                Objects.equals(preinstallGames, that.preinstallGames) &&
+                Objects.equals(battery, that.battery) &&
+                Objects.equals(batteryCapacity, that.batteryCapacity) &&
+                Objects.equals(length, that.length) &&
+                Objects.equals(width, that.width) &&
+                Objects.equals(height, that.height) &&
+                Objects.equals(weight, that.weight) &&
+                Objects.equals(anotherInformation, that.anotherInformation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(formFactor, form, screenDiagonal, screenResolution, architecture, mediaType, memoryCardSlot, preinstallGames, battery, batteryCapacity, length, width, height, weight, anotherInformation);
+    }
 }
--- src/test/java/tests/TabletTest.java
+++ src/test/java/tests/TabletTest.java
@@ -1,10 +1,13 @@
 package tests;
 
 import manager.TestBase;
+import model.RetroConsole;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TabletTest extends TestBase {
@@ -21,6 +24,27 @@ public class TabletTest extends TestBase {
         }
         app.getSpecificationHelper().getWindow(1);
         app.getSearchHelper().clickCharacteristic();
-        Thread.sleep(8000);
+        List<WebElement> list = new ArrayList<>();
+        List<WebElement> characteristics = app.driver.findElements(By.tagName("dd"));
+        for(WebElement i : characteristics){
+            list.add(i);
+        }
+        RetroConsole retro = new RetroConsole().withFormFactor(list.get(1).getText()).withForm(list.get(2).getText()).
+                withScreenDiagonal(list.get(3).getText()).withScreenResolution(list.get(4).getText()).
+                withArchitecture(list.get(5).getText()).withMediaType(list.get(6).getText()).
+                withMemoryCardSlot(list.get(7).getText()).withPreinstallGames(list.get(8).getText()).
+                withBattery(list.get(9).getText()).withBatteryCapacity(list.get(10).getText()).
+                withLength(list.get(11).getText()).withWidth(list.get(12).getText()).withHeight(list.get(13).getText()).
+                withWeight(list.get(14).getText()).withAnotherInformation(list.get(15).getText());
+
+        RetroConsole model = new RetroConsole().withFormFactor("портативная").withForm("моноблок").
+                withScreenDiagonal("4.3 \"").withScreenResolution("480x272").
+                withArchitecture("16-бит").withMediaType("microSD").
+                withMemoryCardSlot("есть").withPreinstallGames("500").
+                withBattery("аккумулятор").withBatteryCapacity("2000 мА·час").
+                withLength("170 мм").withWidth("18 мм").withHeight("82 мм").
+                withWeight("120 г").withAnotherInformation("поддержка игр: NES/FC, GB, GBC, SMS, SMD, SFC/SNES, GBA");
+
+        Assert.assertEquals(model.getScreenDiagonal(), retro.getScreenDiagonal());
     }
 }
